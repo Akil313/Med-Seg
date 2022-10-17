baba7463799008927163dde8a744f05b19fecefd
Akil Hosang <akilhosang@gmail.com>
2022-10-17 02:54:52 -0400

    Implement rest example code

--- med-seg.py
+++ med-seg.py
@@ -59,7 +59,7 @@ masks = np.expand_dims(masks, -1)
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(images, masks, test_size=0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(images[0:100], masks[0:100], test_size=0.25, random_state = 0)
 
 
 image_number = random.randint(0, len(X_train))
@@ -87,4 +87,123 @@ model.compile(optimizer=Adam(learning_rate=1e-3), loss='binary_crossentropy', me
 seed=24
 from keras.preprocessing.image import ImageDataGenerator
 
-y_pred=model.predict(X_train[0])
+img_data_gen_args = dict(rotation_range=90,
+                     width_shift_range=0.3,
+                     height_shift_range=0.3,
+                     shear_range=0.5,
+                     zoom_range=0.3,
+                     horizontal_flip=True,
+                     vertical_flip=True,
+                     fill_mode='reflect')
+
+mask_data_gen_args = dict(rotation_range=90,
+                     width_shift_range=0.3,
+                     height_shift_range=0.3,
+                     shear_range=0.5,
+                     zoom_range=0.3,
+                     horizontal_flip=True,
+                     vertical_flip=True,
+                     fill_mode='reflect',
+                     preprocessing_function = lambda x: np.where(x>0, 1, 0).astype(x.dtype)) #Binarize the output again. 
+
+image_data_generator = ImageDataGenerator(**img_data_gen_args)
+#image_data_generator.fit(X_train, augment=True, seed=seed)
+
+batch_size= 8
+
+image_generator = image_data_generator.flow(X_train, seed=seed, batch_size=batch_size)
+valid_img_generator = image_data_generator.flow(X_test, seed=seed, batch_size=batch_size) #Default batch size 32, if not specified here
+
+mask_data_generator = ImageDataGenerator(**mask_data_gen_args)
+#mask_data_generator.fit(y_train, augment=True, seed=seed)
+mask_generator = mask_data_generator.flow(y_train, seed=seed, batch_size=batch_size)
+valid_mask_generator = mask_data_generator.flow(y_test, seed=seed, batch_size=batch_size)  #Default batch size 32, if not specified here
+
+def my_image_mask_generator(image_generator, mask_generator):
+    train_generator = zip(image_generator, mask_generator)
+    for (img, mask) in train_generator:
+        yield (img, mask)
+
+my_generator = my_image_mask_generator(image_generator, mask_generator)
+
+validation_datagen = my_image_mask_generator(valid_img_generator, valid_mask_generator)
+
+
+x = image_generator.next()
+y = mask_generator.next()
+for i in range(0,1):
+    image = x[i]
+    mask = y[i]
+    plt.subplot(1,2,1)
+    plt.imshow(image[:,:,0], cmap='gray')
+    plt.subplot(1,2,2)
+    plt.imshow(mask[:,:,0])
+    plt.show()
+
+
+steps_per_epoch = 3*(len(X_train))//batch_size
+
+
+history = model.fit_generator(my_generator, validation_data=validation_datagen, 
+                    steps_per_epoch=steps_per_epoch, 
+                    validation_steps=steps_per_epoch, epochs=1)
+
+
+#plot the training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc = history.history['accuracy']
+#acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+#val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, 'y', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+#IOU
+y_pred=model.predict(X_test)
+y_pred_thresholded = y_pred > 0.5
+
+intersection = np.logical_and(y_test, y_pred_thresholded)
+union = np.logical_or(y_test, y_pred_thresholded)
+iou_score = np.sum(intersection) / np.sum(union)
+print("IoU socre is: ", iou_score)
+
+#Predict on a few images
+#model = get_model()
+#model.load_weights('mitochondria_50_plus_100_epochs.hdf5') #Trained for 50 epochs and then additional 100
+#model.load_weights('mitochondria_gpu_tf1.4.hdf5')  #Trained for 50 epochs
+
+test_img_number = random.randint(0, len(X_test))
+test_img = X_test[test_img_number]
+ground_truth=y_test[test_img_number]
+test_img_norm=test_img[:,:,0][:,:,None]
+test_img_input=np.expand_dims(test_img_norm, 0)
+prediction = (model.predict(test_img_input)[0,:,:,0] > 0.2).astype(np.uint8)
+
+plt.figure(figsize=(16, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img[:,:,0], cmap='gray')
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(ground_truth[:,:,0], cmap='gray')
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(prediction, cmap='gray')
+
+plt.show()
