4f1312f730d84caa5ff5f36f3154dd5a329ed645
Akil Hosang <akilhosang@gmail.com>
2022-11-28 03:48:21 -0500

    Added and trained model to generalize to images

--- med-seg-3.py
+++ med-seg-3.py
@@ -1,107 +1,63 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 17 03:39:37 2022
-
-@author: itchi
-"""
-
+import random
 from unet_model import build_unet
 from tensorflow.keras.utils import normalize
-import os
+from tensorflow.keras.callbacks import ModelCheckpoint
+import os, glob, sys
 import cv2
 from PIL import Image
 import numpy as np
-from matplotlib import pyplot as plt
-from patchify import patchify, unpatchify
+import matplotlib.pyplot as plt
 import tifffile as tiff
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.optimizers import Adam
+from patchify import patchify, unpatchify
 
-#12 images only
-large_image_stack = tiff.imread('images/training.tif')
-large_mask_stack = tiff.imread('images/training_groundtruth.tif')
+test_image_shepp = []
+#Read Shepp Loagn image, resize to 256x256, repeat images to perform augmentations and adjust array to fit format [# examples, width, height, 1]
+for filename in sorted(glob.glob('images/s_*.png')):
+  im = cv2.imread(filename, 0)
+  im = cv2.resize(im, (256, 256), interpolation = cv2.INTER_AREA)
+  test_image_shepp.append(im)
 
-large_image_stack = large_image_stack[:12, :, :]
-large_mask_stack = large_mask_stack[:12, :, :]
+test_image_shepp = np.expand_dims(test_image_shepp, -1)
 
-all_img_patches = []
-for img in range(large_image_stack.shape[0]):
-    #print(img)     #just stop here to see all file names printed
-     
-    large_image = large_image_stack[img]
-    
-    patches_img = patchify(large_image, (256, 256), step=256)  #Step=256 for 256 patches means no overlap
-    
+test_image_shepp_mask = []
+#Read Shepp Loagn target, resize to 256x256, repeat images to perform augmentations and adjust array to fit format [# examples, width, height, 1]
+for filename in sorted(glob.glob('images/f_*.png')):
+  im = cv2.imread(filename, 0)
+  im = cv2.resize(im, (256, 256), interpolation = cv2.INTER_AREA)
+  test_image_shepp_mask.append(im)
 
-    for i in range(patches_img.shape[0]):
-        for j in range(patches_img.shape[1]):
-            
-            single_patch_img = patches_img[i,j,:,:]
-            single_patch_img = (single_patch_img.astype('float32')) / 255.
-            #scaler = MinMaxScaler()
-            #single_patch_img= scaler.fit_transform(single_patch_img)
-            
-            all_img_patches.append(single_patch_img)
+test_image_shepp_mask = np.expand_dims(test_image_shepp_mask, -1)
 
-#This will split the image into small images of shape [3,3]
-images = np.array(all_img_patches)
-images = np.expand_dims(images, -1)
-
-all_mask_patches = []
-for img in range(large_mask_stack.shape[0]):
-    #print(img)     #just stop here to see all file names printed
-     
-    large_mask = large_mask_stack[img]
-    
-    patches_mask = patchify(large_mask, (256, 256), step=256)  #Step=256 for 256 patches means no overlap
-    
-
-    for i in range(patches_mask.shape[0]):
-        for j in range(patches_mask.shape[1]):
-            
-            single_patch_mask = patches_mask[i,j,:,:]
-            single_patch_mask = single_patch_mask / 255.
-            
-            all_mask_patches.append(single_patch_mask)
-
-#This will split the image into small images of shape [3,3]
-masks = np.array(all_mask_patches)
-masks = np.expand_dims(masks, -1)
+X_train = X_test = test_image_shepp
+y_train = y_test = test_image_shepp_mask
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(images, masks, test_size = 0.25, random_state = 0)
 
-
-#Sanity check, view few mages
-import random
-import numpy as np
-image_number = random.randint(0, len(X_train))
+#Display the Shep Logan image and the target image
+image_number = random.randint(0, len(X_train)-1)
 plt.figure(figsize=(12, 6))
 plt.subplot(121)
 plt.imshow(np.reshape(X_train[image_number], (256, 256)), cmap='gray')
 plt.subplot(122)
 plt.imshow(np.reshape(y_train[image_number], (256, 256)), cmap='gray')
-plt.show()
+plt.savefig('plots/in_out_img.png')
 
+IMG_HEIGHT = test_image_shepp.shape[1]
+IMG_WIDTH = test_image_shepp.shape[2]
+IMG_CHANNELS = test_image_shepp.shape[3]
 
-
-IMG_HEIGHT = images.shape[1]
-IMG_WIDTH  = images.shape[2]
-IMG_CHANNELS = images.shape[3]
-
-
+#Build model from unet_model structure
 input_shape = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
-
 model = build_unet(input_shape)
-model.compile(optimizer=Adam(lr = 1e-3), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=1e-3), loss='binary_crossentropy', metrics=['accuracy'])
 model.summary()
 
-
-#New generator with rotation and shear where interpolation that comes with rotation and shear are thresholded in masks. 
-#This gives a binary mask rather than a mask with interpolated values. 
 seed=24
 from keras.preprocessing.image import ImageDataGenerator
 
+#Define dictionary listing augmentations to perform on input data
 img_data_gen_args = dict(rotation_range=90,
                      width_shift_range=0.3,
                      height_shift_range=0.3,
@@ -111,6 +67,7 @@ img_data_gen_args = dict(rotation_range=90,
                      vertical_flip=True,
                      fill_mode='reflect')
 
+#Define dictionary listing augmentations to perform on target data
 mask_data_gen_args = dict(rotation_range=90,
                      width_shift_range=0.3,
                      height_shift_range=0.3,
@@ -119,10 +76,10 @@ mask_data_gen_args = dict(rotation_range=90,
                      horizontal_flip=True,
                      vertical_flip=True,
                      fill_mode='reflect',
-                     preprocessing_function = lambda x: np.where(x>0, 1, 0).astype(x.dtype)) #Binarize the output again. 
+                     preprocessing_function = lambda x: np.where(x>1, 1, 0).astype(x.dtype)) #Binarize the output again.
 
+#Apply image generator to input and target/mask samples
 image_data_generator = ImageDataGenerator(**img_data_gen_args)
-#image_data_generator.fit(X_train, augment=True, seed=seed)
 
 batch_size= 8
 
@@ -130,7 +87,6 @@ image_generator = image_data_generator.flow(X_train, seed=seed, batch_size=batch
 valid_img_generator = image_data_generator.flow(X_test, seed=seed, batch_size=batch_size) #Default batch size 32, if not specified here
 
 mask_data_generator = ImageDataGenerator(**mask_data_gen_args)
-#mask_data_generator.fit(y_train, augment=True, seed=seed)
 mask_generator = mask_data_generator.flow(y_train, seed=seed, batch_size=batch_size)
 valid_mask_generator = mask_data_generator.flow(y_test, seed=seed, batch_size=batch_size)  #Default batch size 32, if not specified here
 
@@ -143,26 +99,38 @@ my_generator = my_image_mask_generator(image_generator, mask_generator)
 
 validation_datagen = my_image_mask_generator(valid_img_generator, valid_mask_generator)
 
-
+#Display augmented input and mask samples
 x = image_generator.next()
 y = mask_generator.next()
-for i in range(0,1):
+
+test_mask_1 = y[1,:,:]
+
+for i in range(0,5):
     image = x[i]
     mask = y[i]
     plt.subplot(1,2,1)
     plt.imshow(image[:,:,0], cmap='gray')
     plt.subplot(1,2,2)
     plt.imshow(mask[:,:,0])
-    plt.show()
-
+    plt.title('Augmented Image and Augmented Mask')
+    plt.savefig('plots/aug_pair_{}.png'.format(i))
 
 steps_per_epoch = 3*(len(X_train))//batch_size
 
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
 history = model.fit(my_generator, validation_data=validation_datagen, 
-                    steps_per_epoch=steps_per_epoch, 
-                    validation_steps=steps_per_epoch, epochs=25)
+                      steps_per_epoch=steps_per_epoch, 
+                      validation_steps=steps_per_epoch, epochs=20,
+                      callbacks=[cp_callback])
 
+model.save_weights('models/medseg_weights.h5')
 
 #plot the training and validation accuracy and loss at each epoch
 loss = history.history['loss']
@@ -174,12 +142,10 @@ plt.title('Training and validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
-plt.show()
+plt.savefig('plots/train_val_loss.png')
 
 acc = history.history['accuracy']
-#acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
-#val_acc = history.history['val_accuracy']
 
 plt.plot(epochs, acc, 'y', label='Training acc')
 plt.plot(epochs, val_acc, 'r', label='Validation acc')
@@ -187,7 +153,7 @@ plt.title('Training and validation accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
-plt.show()
+plt.savefig('plots/train_val_accuracy.png')
 
 #IOU
 y_pred=model.predict(X_test)
@@ -198,17 +164,14 @@ union = np.logical_or(y_test, y_pred_thresholded)
 iou_score = np.sum(intersection) / np.sum(union)
 print("IoU socre is: ", iou_score)
 
-#Predict on a few images
-#model = get_model()
-#model.load_weights('mitochondria_50_plus_100_epochs.hdf5') #Trained for 50 epochs and then additional 100
-#model.load_weights('mitochondria_gpu_tf1.4.hdf5')  #Trained for 50 epochs
-
-test_img_number = random.randint(0, len(X_test))
+test_img_number = random.randint(0, len(X_test)-1)
 test_img = X_test[test_img_number]
 ground_truth=y_test[test_img_number]
 test_img_norm=test_img[:,:,0][:,:,None]
 test_img_input=np.expand_dims(test_img_norm, 0)
-prediction = (model.predict(test_img_input)[0,:,:,0] > 0.2).astype(np.uint8)
+
+
+prediction = (model.predict(test_img_input) > 0.2).astype(np.uint8)
 
 plt.figure(figsize=(16, 8))
 plt.subplot(231)
@@ -219,6 +182,5 @@ plt.title('Testing Label')
 plt.imshow(ground_truth[:,:,0], cmap='gray')
 plt.subplot(233)
 plt.title('Prediction on test image')
-plt.imshow(prediction, cmap='gray')
-
-plt.show()
+plt.imshow(prediction[0, :, :, 0], cmap='gray')
+plt.savefig('plots/output_comparrison.png')
